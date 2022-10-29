b91a096fdd13945757430619c6471f5efd3c4e7f
orangeguseul <orangeguseul@gmail.com>
2022-10-30 00:03:49 +0900

    header

--- src/App.svelte
+++ src/App.svelte
@@ -2,7 +2,7 @@
 import Header from "./head.svelte";
 </script>
 <main class="bg-gray-700 grid justify-center h-screen">
-  <div class="text-center bg-[#1a1e23] w-96">
+  <div class="bg-[#1a1e23] w-96">
     <Header/>
   </div>
 </main>
--- src/head.svelte
+++ src/head.svelte
@@ -1,3 +1,11 @@
 <div class="mt-0 w-auto h-5">
-	<h1 class="mr-5">학교 시간표</h1>
+  <nav class="inline-block h-10 w-96">
+    <h1
+      class="ml-10 text-white text-xl mt-4"
+      style="font-family: 'Noto Sans KR', sans-serif;"
+    >
+	 중동중학교
+    </h1>
+    <img src="settings.png" alt="설정" />
+  </nav>
 </div>
