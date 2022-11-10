a84f6accda9338e4059b0b17350ff3cb90b3631a
orangeguseul <orangeguseul@gmail.com>
2022-11-10 12:43:39 +0900

    add title

--- src/App.svelte
+++ src/App.svelte
@@ -2,7 +2,14 @@
 import Header from "./head.svelte";
 </script>
 <main class="bg-gray-700 grid justify-center h-screen">
-  <div class="bg-[#1a1e23] w-96">
+  <div class="bg-[#1a1e23] w-screen sm:w-96">
     <Header/>
+    <div class="grid justify-items-center h-5/6">
+      <div class="bg-gray-700 text-center mt-10 w-4/5 rounded-2xl text-white">
+        <div class="grid justify-center">
+          <div class="bg-[#1a1e23] w-1/5 h-2.5">제목임</div>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos id eius illum, consequatur accusantium quaerat impedit? Architecto id tempora quod, animi et molestias dignissimos vitae laudantium, quia nemo debitis amet.
+        </div>
+    </div>    
   </div>
 </main>
--- src/head.svelte
+++ src/head.svelte
@@ -1,11 +1,15 @@
 <div class="mt-0 w-auto h-5">
-  <nav class="inline-block h-10 w-96">
+  <nav class="h-10 w-screen flex justify-between mt-4 items-center sm:w-96">
     <h1
-      class="ml-10 text-white text-xl mt-4"
+      class="ml-10 text-white text-xl"
       style="font-family: 'Noto Sans KR', sans-serif;"
     >
-	 중동중학교
+      00중학교
     </h1>
-    <img src="settings.png" alt="설정" />
+    <img
+      src="settings.svg"
+      class="w-6 h-6  ml-5 mr-10 content-center"
+      alt="설정"
+    />
   </nav>
 </div>
